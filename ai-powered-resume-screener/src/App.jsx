0b339eaee1880a641a111b499d3c31a9bf29ad11
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-10 00:43:16 +0530

    Delete ai-powered-resume-screener/src/App.jsx

--- ai-powered-resume-screener/src/App.jsx
+++
@@ -1,8 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center text-2xl text-center p-6">
-      🚀 AI powered resume screener
-    </div>
-  );
-}
-export default App;
